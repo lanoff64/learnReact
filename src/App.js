b617e001dad6df8fa44186dcd1ae56e5cde3cd40
lanoff64 <lanov64@gmail.com>
2021-07-11 23:58:42 +0600

    commentTest

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
           <p>
-              Hello world!!!
+              {/*Hello world!!!*/}
           </p>
         <img src={logo} className="App-logo" alt="logo" />
       </header>

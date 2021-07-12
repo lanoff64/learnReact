e7c684af214505e81fcedf7f88e0abfa7900fa08
lanoff64 <lanov64@gmail.com>
2021-07-13 02:02:44 +0600

    add class components Clock

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import './App.css';
 import HelloWorld from "./components/HelloWorld/HelloWorld";
+import Clock from "./components/Clock/Clock";
+
+
+
+
 const App = () => {
   return (
     <div className="App-wrapper">
 
-
         <HelloWorld/>
-
+        <Clock/>
 
 
     </div>
